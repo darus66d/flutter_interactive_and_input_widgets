3bd364f46a7e767aa1c89056dfad2ef3be381ad0
Darus Salam <darussalam66d@gmail.com>
2025-10-09 11:22:17 +0600

    Outlined_button

--- lib/main.dart
+++ lib/main.dart
@@ -26,34 +26,55 @@ class Homepage extends StatelessWidget {
         title: Center(child: Text("TextButton",style: TextStyle(color: Colors.white),)),
       ),
       // backgroundColor: Colors.deepPurple,
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children:[
-              OutlinedButton(onPressed: (){
-                print("Outlined Button");
-              }, child: Text("Outline Button"),
-                style: OutlinedButton.styleFrom(
-                  side: BorderSide(color: Colors.red,width: 2),
-                  padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(1)
-                  )
-                ),
-              ),
-              SizedBox(height: 10,),
-              OutlinedButton(onPressed: (){
-                print("Outlined Button");
-              }, child: Icon(Icons.home),
-                style: OutlinedButton.styleFrom(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Row(
+              children:[
+                OutlinedButton(onPressed: (){
+                  print("Outlined Button");
+                }, child: Text("Outline Button"),
+                  style: OutlinedButton.styleFrom(
                     side: BorderSide(color: Colors.red,width: 2),
                     padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
-                    shape: CircleBorder()
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(1)
                     )
+                  ),
                 ),
-            ],
-        ),
+                SizedBox(height: 10,),
+                OutlinedButton(onPressed: (){
+                  print("Outlined Button");
+                }, child: Icon(Icons.home),
+                  style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Colors.red,width: 2),
+                      padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
+                      shape: CircleBorder()
+                      )
+                  ),
+                SizedBox(height: 10,),
+                OutlinedButton(onPressed: (){
+                  print("Outlined Button");
+                }, child: Icon(Icons.delete),
+                  style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Colors.red,width: 2),
+                      padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
+                      shape: CircleBorder()
+                      )
+                  ),SizedBox(height: 10,),
+                OutlinedButton(onPressed: (){
+                  print("Outlined Button");
+                }, child: Icon(Icons.delete_sweep_outlined),
+                  style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Colors.red,width: 2),
+                      padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
+                      shape: CircleBorder()
+                      )
+                  ),
+              ],
+          ),
+        ],
       )
     );
   }
