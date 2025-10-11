73c122a2ac9a942124932de75f39375e9b683cbb
Darus Salam <darussalam66d@gmail.com>
2025-10-11 10:51:48 +0600

    popupmenu button

--- lib/popupmenu_button.dart
+++ lib/popupmenu_button.dart
@@ -17,51 +17,6 @@ class MyApp extends StatelessWidget {
 }
 
 class Homepage extends StatelessWidget {
-  // const Homepage({super.key});
-  List<String> cityList=['Dhaka','ctg','khulna'];
-  // String selectedValue = 'Khulna';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.indigo,
-        title: Center(child: Text("Drop Down Button",style: TextStyle(color: Colors.white),)),
-      ),
-      body: PopupMenuButton(
-          icon: Icon(Icons.more_outlined),
-          itemBuilder:(BuildContext context)=>[
-            PopupMenuItem(
-                child: Text('Edit')), PopupMenuItem(
-                child: Text('Delete')), PopupMenuItem(
-                child: Text('Share')),
-          ]
-      ),
-    );
-  }
-}
-
-
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Homepage(),
-    );
-  }
-}
-
-class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
 
