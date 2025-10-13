02a911addcb7df451f1f08d32f07a872491c0557
Darus Salam <darussalam66d@gmail.com>
2025-10-13 11:42:36 +0600

    Grid_View

---
+++ lib/List and scroll widgets/grid_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GridViewPage extends StatelessWidget {
+  const GridViewPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.black12,
+        appBar: AppBar(
+          backgroundColor: Colors.indigo,
+          title: Center(child: Text("Grid view",style: TextStyle(color: Colors.white),)),
+        ),
+        body: GridView.count(
+            crossAxisCount: 2,
+          childAspectRatio: 1,
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 8,
+          children: [
+            Container(color: Colors.red,),
+            Container(color: Colors.green,),
+            Container(color: Colors.blue,),
+            Container(color: Colors.purple,),
+          ],
+        )
+
+    );
+  }
+}
