16f5feb6c40c65c6c92441903c72200072578a2d
Darus Salam <darussalam66d@gmail.com>
2025-10-13 10:28:10 +0600

    Scroll

--- lib/List and scroll widgets/scroll.dart
+++ lib/List and scroll widgets/scroll.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 
 class ScrollPage extends StatelessWidget {
   const ScrollPage({super.key});
-
   @override
-
-
 
 
   Widget build(BuildContext context) {
