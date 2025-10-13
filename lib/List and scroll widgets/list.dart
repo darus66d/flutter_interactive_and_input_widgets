24a7750ec2e15e959e952a4966f04f379e8b22b4
Darus Salam <darussalam66d@gmail.com>
2025-10-13 11:36:38 +0600

    List

--- lib/List and scroll widgets/list.dart
+++ lib/List and scroll widgets/list.dart
@@ -10,47 +10,22 @@ class ListPage extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black12,
+      // backgroundColor: Colors.black12,
       appBar: AppBar(
         backgroundColor: Colors.indigo,
         title: Center(child: Text("List_Widgets",style: TextStyle(color: Colors.white),)),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(color: Colors.blueGrey, height: 200,),
-            Container(color: Colors.yellow, height: 200,),
-            Container(color: Colors.green, height: 200,),
-            Container(
-              color: Colors.blueGrey,
-              height: 200,
-              child: ListView(
-                // shrinkWrap: true,
-                // scrollDirection: Axis.horizontal,
-                children: [
-                  ListTile(title: Text("Item 1"),),
-                  ListTile(title: Text("Item 2"),),
-                  ListTile(title: Text("Item 3"),),
-                  ListTile(title: Text("Item 4"),),
-                  ListTile(title: Text("Item 5"),),
-                  // Container(color: Colors.blueGrey, height: 200,),
-                  // Container(color: Colors.yellow, height: 200,),
-                  // Container(color: Colors.green, height: 200,),
-                  // Container(color: Colors.blue, height: 200,),
-                  // Container(color: Colors.black, height: 200,),
-                  // Container(color: Colors.blueGrey, width:150,),
-                  // Container(color: Colors.yellow, width: 50,),
-                  // Container(color: Colors.green, width:250,),
-                  // Container(color: Colors.blue, width: 50,),
-                  // Container(color: Colors.black, width: 150,),
-                  // Container(color: Colors.purple, width: 150,),
-                  // Container(color: Colors.blueGrey, width:150,),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: ListView.builder(
+        itemCount: 500,
+        itemBuilder: (context,index){
+          if(index==200){
+            return ListTile(title: Text("Item #${index+1}",
+              style: TextStyle(color: Colors.blue),),);
+          }else{
+          return ListTile(title: Text("Item #${index+1}"),);
+        }
+        }
+      )
 
     );
   }
